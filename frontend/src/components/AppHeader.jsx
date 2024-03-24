d6a3677ed88826d2e37eaa9ec36b12ef0849a031
DiptiPrabhavale11 <d_prabhavale@u.pacific.edu>
2024-03-24 10:45:50 -0700

    Added icon for logout

--- frontend/src/components/AppHeader.jsx
+++ frontend/src/components/AppHeader.jsx
@@ -2,21 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { LinkContainer } from 'react-router-bootstrap';
-import {Button} from "react-bootstrap";
+import * as Icon from 'react-bootstrap-icons';
 
-const AppHeader = ({user,setUser}) => {
-    const handleLogout=()=>{
+const AppHeader = ({ user, setUser }) => {
+    const handleLogout = () => {
         window.localStorage.clear();
         setUser(null);
     }
     return (
         <>
-            <Navbar variant="dark" fixed="top" style={{ width:'100%', height:'90px', backgroundColor: '#545c81', color: '#fff' , padding: '0px'}}>
+            <Navbar variant="dark" fixed="top" style={{ width: '100%', height: '90px', backgroundColor: '#545c81', color: '#fff', padding: '0px' }}>
                 <Container>
-                    <Navbar.Brand style={{width:'80%'}}>
-                        <h2 style={{fontSize: '30px' }}>Logs Monitor</h2>
+                    <Navbar.Brand style={{ width: '80%' }}>
+                        <h2 style={{ fontSize: '30px' }}>Logs Monitor</h2>
                     </Navbar.Brand>
-                    <Nav className="me-auto" style={{ color: 'white'}}>
+                    {user && <Nav className="me-auto" style={{ color: 'white' }}>
                         <LinkContainer to="/logs" >
                             <Nav.Link>Logs</Nav.Link>
                         </LinkContainer>
@@ -26,9 +26,9 @@ const AppHeader = ({user,setUser}) => {
                         <LinkContainer to="/about">
                             <Nav.Link>About</Nav.Link>
                         </LinkContainer>
-                    </Nav>
+                    </Nav>}
                 </Container>
-                {user && <Button onClick={handleLogout} variant="dark" style={{alignSelf: 'flex-end',backgroundColor:'#a25151'}}>Logout</Button>}
+                {user && <Icon.BoxArrowRight onClick={handleLogout} size={20} className='mx-5'/>}
             </Navbar>
         </>
     )
